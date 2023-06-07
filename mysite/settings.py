67c37423befc5ea76551ace78e47252d2c8cc587
franciscomartinezespartero <francisco.martinez@ciberexperis.es>
2023-06-07 14:49:30 +0200

    heroku config

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,3 +120,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+import django_heroku
+django_heroku.settings(locals())
